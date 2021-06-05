5c49d58b86c5bd1053e7ac9dd464328a9d5221d2
NIYATI <niyupathak6@gmail.com>
2021-06-05 21:56:09 +0530

    update on 5-6-21 by niyati

--- SRMS/settings.py
+++ SRMS/settings.py
@@ -93,10 +93,21 @@ WSGI_APPLICATION = 'SRMS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}"""
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SRMS',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
